3b25de7fb8db32a4fe9f04b5c1d1f835bd756ca6
Kelechi Okoli <kelechio1992@gmail.com>
2022-12-02 15:07:25 +0000

    db now connected successfully to express

--- db/init.js
+++ db/init.js
@@ -1,14 +1,14 @@
 const dotenv = require('dotenv').config()
-const {Client} = require('pg')
+const {Client, Pool} = require('pg')
 const fs = require('fs');
 
+const seeds = fs.readFileSync(__dirname + '/seed.sql').toString()
+
 const db = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    connectionString: process.env.DATABASE_URL
 })
 
 db.connect(err => err ? console.error('connection error!', err.stack) : console.log('postgres db conntected!'))
+db.query(seeds, () => console.log('database seeded'))
 
 module.exports = {db}
