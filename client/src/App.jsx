8a0432660a7e79b84a1396b7f546d14536764a41
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-12-02 09:55:54 +0000

    Boilerplate changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { NavBar } from './components'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <NavBar/>
     </div>
   )
 }
---
+++ client/src/components/NavBar/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () =>{
+    return(
+        <div id="navBar">
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default NavBar;
