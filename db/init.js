5786324c8b754146b6ee9b7e60f9bf5a22478915
Kelechi Okoli <kelechio1992@gmail.com>
2022-12-02 15:22:36 +0000

    fixed bug with docker and db connection

--- db/init.js
+++ db/init.js
@@ -8,7 +8,4 @@ const db = new Client({
     connectionString: process.env.DATABASE_URL
 })
 
-db.connect(err => err ? console.error('connection error!', err.stack) : console.log('postgres db conntected!'))
-db.query(seeds, () => console.log('database seeded'))
-
-module.exports = {db}
+module.exports = {db, seeds}
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const dotenv = require('dotenv').config()
 const app = require('./server')
+const {db, seeds} = require('./db/init')
 const port = process.env.PORT || 8080
 
-
-app.listen(port, () => console.log(`> goodREACTion online @ http://localhost:${port}`))
+app.listen(port, () => {
+    db.connect(err => err ? console.error('connection error!', err.stack) : console.log('postgres db conntected!'))
+    db.query(seeds, () => console.log('database seeded'))
+    console.log(`> goodREACTion online @ http://localhost:${port}`)
+})
