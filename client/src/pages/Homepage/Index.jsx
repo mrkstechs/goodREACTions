0c1c998041f8eb058c80f7f76fbf8ea701df6963
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-02 16:29:43 +0000

    Moved socket stuff to lobby page

--- client/src/pages/Homepage/Index.jsx
+++ client/src/pages/Homepage/Index.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { NavBar } from '../../components'
-import { io } from 'socket.io-client'
 
 const Homepage = () =>{
-
-    const socket = io('http://localhost:2333')
-
 
     return (
         <>
