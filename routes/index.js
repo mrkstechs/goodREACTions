b9b1ac7026b08a63510d654a82668acda8884a7e
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-02 11:01:08 +0000

    Working on highscore routes

--- routes/index.js
+++ routes/index.js
@@ -4,33 +4,37 @@ const router = express.Router()
 const db = require("../db/dbConfig")
 
 router.get('/', (req, res) => {
-    res.status(200).res.json({message: 'hello, world'})
+    res.status(200).res('hello, world')
 })
 
 router.get('/highscores', (req, res) => {
-    return new Promise (async (resolve, reject) => {
+    res.status(200).json(
+        new Promise (async (resolve, reject) => {
         try {
             let scoreData = await db.query(`SELECT * FROM highscores`)
             // Think this might need a model
             resolve(scoreData.rows)
         } catch (err) {
-            reject(`Error adding highscore to database: ${err}`)
+            reject(`Error getting highscores: ${err}`)
         }
     })
+    )
 })
 
 router.post('/highscores', (req, res) => {
     let name = req.body.name
     let highscore = req.body.highscore
 
-    return new Promise (async (resolve, reject) => {
-        try {
-            await db.query(`INSERT INTO highscores (name, highscore) VALUES ($1, $2);`, [ name, highscore ])
-            resolve("Added successfully")
-        } catch (err) {
-            reject(`Error adding highscore to database: ${err}`)
-        }
-    })
+    res.status(200).json(
+        new Promise (async (resolve, reject) => {
+            try {
+                await db.query(`INSERT INTO highscores (name, highscore) VALUES ($1, $2);`, [ name, highscore ])
+                resolve("Added successfully")
+            } catch (err) {
+                reject(`Error adding highscore to database: ${err}`)
+            }
+        })
+    )
 })
 
 module.exports = router
