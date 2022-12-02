fbe2f9872b6b5652822a5139d05cd212aa8f443a
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-12-02 11:20:50 +0000

    Navbar so far

--- client/src/components/NavBar/Index.jsx
+++ client/src/components/NavBar/Index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import { FaBars } from 'react-icons/fa'
+
 const NavBar = () =>{
     return(
         <div id="navBar">
             <h1>Untitled Quiz Game</h1>
+            <div id="icon">
+                    <FaBars />
+                </div>
         </div>
     )
 }
