52066d8b6b8574c091654db7f01816d778104e81
“abdul-san” <kamara847@gmail.com>
2022-12-05 15:48:03 +0000

    question page draft

--- client/src/pages/Questionpage/Index.jsx
+++ client/src/pages/Questionpage/Index.jsx
@@ -4,6 +4,18 @@ const Questionpage = () =>{
     return(
         <>
         <h1>Question page</h1>
+        <div className="quizbody">
+        <div id='question'>
+        <p >question</p> 
+        </div>
+        
+        <div id='quizbtns'>     
+            <button id='quizbtn'>btn1</button>
+            <button id='quizbtn'>btn2</button>
+            <button id='quizbtn'>btn3</button>
+            <button id='quizbtn'>btn4</button>
+        </div>  
+        </div>
         </>
     )
 }
