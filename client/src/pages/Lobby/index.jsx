4ac1d206c308d0e04b8f00c598c8d7ccf1501c88
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-05 09:30:22 +0000

    Working on lobby creation

--- client/src/pages/Lobby/index.jsx
+++ client/src/pages/Lobby/index.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { io } from 'socket.io-client'
 
 const Lobby = () => {
 
     const [lobbyId, setLobbyId] = useState()
 
-    const socket = io('http://localhost:2333')
+    useEffect(() => {
+        const socket = io('http://localhost:2333')
 
-    socket.on("connect", () => {
-        console.log(`Connected to socket server with client id: ${socket.id}`)
-
-        if (!lobbyId) {
-            console.log("No lobby specified. Creating new lobby...")
-            setLobbyId(socket.id)
-        }
-
-        socket.emit('join-lobby', lobbyId, response => {console.log(response)})
+        socket.on("connect", () => {
+            console.log(`Connected to socket server with client id: ${socket.id}`)
     
-    })
+            if (!lobbyId) {
+                console.log("No lobby specified. Creating new lobby...")
+                setLobbyId(socket.id)
+            }
+
+            socket.emit('join-lobby', lobbyId, response => {console.log(response)})
+                
+        })
+    
+
+    }, [])
+
 
     
     function startGame() {
