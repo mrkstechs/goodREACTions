95bf845a44cf3d7eba833d167135597da110f7d3
Kelechi Okoli <kelechio1992@gmail.com>
2022-12-05 16:24:53 +0000

    leaderboard data shows on UI

--- client/src/pages/Leaderboardpage/Index.jsx
+++ client/src/pages/Leaderboardpage/Index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useUpdateAppState } from '../../context'
 const Leaderboardpage = () =>{
+    const [state, setState ] = useUpdateAppState()
     return(
         <>
         <h1>Leaderboard page</h1>
+        {state.leaderboard.map((user, index) => <p key={index}>{`User: ${user.name}, Score: ${user.score}`}</p>)}
         </>
     )
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { getScores, addScore } = require('../controllers')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.status(200).res.json({message: 'hello, world'})
+    res.status(200).json({message: 'hello, world'})
 })
 
 router.get('/highscores', (req, res) => {
