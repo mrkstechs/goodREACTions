b3a6d31231116c5ef92aa34ede7d966bfebaf514
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-12-05 16:28:20 +0000

    homepage changes

--- client/src/pages/Homepage/Index.jsx
+++ client/src/pages/Homepage/Index.jsx
@@ -1,19 +1,27 @@
-import React, {  useEffect, useState  } from 'react'
+import React, {  useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Popup from 'reactjs-popup'
 
 const Homepage = () =>{
-    const [active, setActive] = useState(false)
+    
 
     const navigate = useNavigate()
 
-    function toLobby(){
-        navigate('/lobby')
-    }
+    // function toLobby(){
+    //     navigate('/lobby')
+    // }
 
     function toLeaderboard(){
         navigate('/leaderboard')
     }
+
+    // function createGame(){
+        //store username and lobbyid, create new 'session'.
+    // }
+
+    // function joinGame(){
+    //     // if game id = input value, send to lobby, else, error message.
+    // }
 
     return (
         <>
@@ -21,13 +29,28 @@ const Homepage = () =>{
         <div className='background'>
             {/* logo component */}
             <div id='homebtns'>
-                <button onClick = {toLobby}>Create Lobby</button>
-                <Popup trigger={<button>Join Lobby</button>}>
-                    <div id='popupForm'>
-                        <form>
+                <Popup trigger={<button>Create Lobby</button>}>
+                    <div className='popupForm'>
+                        <h2>Create Lobby</h2>
+                        <form id='popupCreate'>
+                            <label htmlFor="username">User Name:</label>
+                            <input type="text" name='username' placeholder='Enter Lobby name here...' className='txt'/>
                             <label htmlFor="lobby">Lobby Name:</label>
-                            <input type="text" name='lobby' placeholder='Enter Lobby name here...' id='txt'/>
-                            <input type="button" value="Enter" id='enter'/>
+                            <input type="text" name='lobby' placeholder='Enter Lobby name here...' className='txt'/>
+                            <input type="button" value="Enter" className='enter'/>
+                        </form>
+                    </div>
+                </Popup>
+
+                <Popup trigger={<button>Join Lobby</button>}>
+                    <div className='popupForm'>
+                        <h2>Join Lobby</h2>
+                        <form id='popupJoin'>
+                            <label htmlFor="username">User Name:</label>
+                            <input type="text" name='username' placeholder='Enter Lobby name here...' className='txt'/>
+                            <label htmlFor="lobby">Lobby Name:</label>
+                            <input type="text" name='lobby' placeholder='Enter Lobby name here...' className='txt'/>
+                            <input type="button" value="Enter" className='enter'/>
                         </form>
                     </div>
                 </Popup>
