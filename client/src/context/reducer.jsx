573ab0874b5df7352f9c6c6df359346eaf2271b3
Kelechi Okoli <kelechio1992@gmail.com>
2022-12-05 12:55:02 +0000

    added models, controllers, app context & tested it works

---
+++ client/src/context/reducer.jsx
@@ -0,0 +1,14 @@
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'UPDATE_LEADERBOARD':
+            return { ...state, leaderboard: action.payload }
+        case 'UPADTE_ACTIVE_PLAYERS':
+            return { ...state, active_players: action.payload }
+        case 'UPDATE_CURRENT_SESSIONS':
+            return { ...state, current_sessions: action.payload }
+        default:
+            return state
+    }
+}
+
+export default reducer
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
+import { AppProvider } from './context'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AppProvider>
+      <Router>
+        <App />
+      </Router>
+    </AppProvider>
   </React.StrictMode>
 )
