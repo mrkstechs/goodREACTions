a166e43627986ce7cd97b9ad9d76288b3564b769
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-12-06 17:13:21 +0000

    tests for homepage and app

--- client/src/pages/Homepage/Homepage.test.js
+++ client/src/pages/Homepage/Homepage.test.js
@@ -1,1 +1,31 @@
+import { screen, render } from '@testing-library/react';
+import Homepage from '.'
+import {BrowserRouter as Router} from 'react-router-dom';
 
+describe('Homepage', () =>{
+    beforeEach(() =>{
+        render(<Homepage />, { wrapper: Router } )
+    })
+
+    test('It has button to create lobby', () =>{
+        const createLobby = screen.getByRole('button', { name: "Create Lobby"})
+        expect(createLobby).toBeInTheDocument()
+        expect(createLobby.textContent).toBe("Create Lobby")
+    })
+
+    test('It has button to join lobby', () =>{
+        const joinLobby = screen.getByRole('button', { name: "Join Lobby"})
+        expect(joinLobby).toBeInTheDocument()
+        expect(joinLobby.textContent).toBe("Join Lobby")
+    })
+
+    test('It has button to see Leaderboard', () =>{
+        const leaderboardLobby = screen.getByRole('button', { name: "Leaderboard"})
+        expect(leaderboardLobby).toBeInTheDocument()
+        expect(leaderboardLobby.textContent).toBe("Leaderboard")
+    })
+
+    test('It has a form', () =>{
+        
+    })
+})
--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,31 +1,50 @@
-// import App from '../App.jsx'
-// import Enzyme from 'enzyme';
-// import Adapter from '@cfaester/enzyme-adapter-react-18';
-// import { shallow, mount } from 'enzyme';
-// import { screen, render } from '@testing-library/react';
-// import { Route, Routes } from 'react-router-dom';
+import App, { NavBar, Homepage, Lobby, Leaderboardpage, Questionpage } from '../App.jsx'
+import Enzyme from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+import { shallow, mount } from 'enzyme';
+import { screen, render } from '@testing-library/react';
+import { Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-// Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
-// describe('App', () =>{
-//     beforeEach(() =>{
-//         render(<App/>)
-//     });
-// })
+describe('App', () =>{
+    beforeEach(() =>{
+        render(<App/>, { wrapper: Router } )
+    });
+})
 
-// let pathMap = {}
-// describe('routes', () =>{
-//     beforeAll(() =>{
-//         const component = shallow(<Routes/>)
-//         pathMap = component.find(Route).reduce((pathMap, route) =>{
-//             const routeProps = route.props();
-//             pathMap[routeProps.path] = routeProps.component
-//             return pathMap;
-//         }, {})
-//         console.log(pathMap)
-//     })
+let pathMap = {}
+describe('routes', () =>{
+    beforeAll(() =>{
+        const component = shallow(<Router><Routes/></Router> )
+        pathMap = component.find(Route).reduce((pathMap, route) =>{
+            const routeProps = route.props();
+            pathMap[routeProps.path] = routeProps.component
+            return pathMap;
+        }, {})
+        console.log(pathMap)
+    })
 
-//     test('Should show NavBar page for / router', () =>{
-//         expect(pathMap['/']).toBe(NavBar)
-//     })
-// })
+    test('Should show NavBar page for / route', () =>{
+        expect(pathMap['/']).toBe(NavBar)
+    })
+    test('Should show Homepage page for / route', () =>{
+        expect(pathMap['/']).toBe(Homepage)
+    })
+    test('Should show Lobby page for /lobby route', () =>{
+        expect(pathMap['/lobby']).toBe(Lobby)
+    })
+    test('Should show leaderboard page for /leaderboard route', () =>{
+        expect(pathMap['/leaderboard']).toBe(Leaderboardpage)
+    })
+    test('Should show Question page for /question route', () =>{
+        expect(pathMap['/question']).toBe(Questionpage)
+    })
+    test('Should show Error message for other routes', () =>{
+        // let element = document.createElement("h1")
+        // element.textContent = "404"
+        expect(pathMap['undefined']).toBe(undefined)
+    })
+    
+})
