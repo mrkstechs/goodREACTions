4bfa680c4e43d735eed03cec35568aefbcd45db4
Kelechi Okoli <kelechio1992@gmail.com>
2022-12-06 13:46:14 +0000

    added logic and components for leaderboard page

--- client/src/pages/Leaderboardpage/Index.jsx
+++ client/src/pages/Leaderboardpage/Index.jsx
@@ -1,11 +1,30 @@
 import React, { useEffect, useState } from 'react'
+import { Podium } from '../../components'
+import RunnerUps from '../../components/RunnerUps'
 import { useUpdateAppState } from '../../context'
+
 const Leaderboardpage = () =>{
     const [state, setState ] = useUpdateAppState()
+    const [sorted, setSorted] = useState(false)
+
+    useEffect(() => { 
+        if(state.leaderboard.length !== 0){
+            setSorted(true)
+        }
+    }, [state, sorted])
+    
+    const topFive = (arr) => {
+        const sorted = arr.sort((a, b) => b.score - a.score)
+        return sorted.map((user, index) => (
+            index <= 2 ? <Podium key={index} name={user.name}/> : 
+            index > 2 && index <= 4 ? <RunnerUps key={index} name={user.name} /> : ''
+        ))
+    }
+
     return(
         <>
-        <h1>Leaderboard page</h1>
-        {state.leaderboard.map((user, index) => <p key={index}>{`User: ${user.name}, Score: ${user.score}`}</p>)}
+        <h1>Leaderboard Page</h1>
+        { sorted && topFive(state.leaderboard) }
         </>
     )
 }
