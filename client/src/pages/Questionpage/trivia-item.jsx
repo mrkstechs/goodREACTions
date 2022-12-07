77c9647c11ba924fc0fae2715696481d7a9dbb74
“abdul-san” <kamara847@gmail.com>
2022-12-07 14:49:44 +0000

    added package to handle html encoding

--- client/src/pages/Questionpage/trivia-item.jsx
+++ client/src/pages/Questionpage/trivia-item.jsx
@@ -1,17 +1,21 @@
+import shuffle from "./shuffle"
+
 function TriviaItem({ correctAnswer, incorrectAnswers, question }) {
+    const allAnswers = [correctAnswer, ...incorrectAnswers];
+    const shuffledAnswers = shuffle(allAnswers)
+
     return <div>
         <p className="trivia-item__question">{question}</p>
         <ul className="trivia-item__answers">
-            <li>
-                <button id="trivia-item__button">{correctAnswer}</button>
+            {shuffledAnswers.map((answer, i ) => {
+
+                return(
+                    <li key={answer}>
+                <button id="trivia-item__button">{answer}</button>
             </li>
-            <li>
-                <button id="trivia-item__button">{incorrectAnswers[0]}</button>
-            </li>
-            <li>
-                <button id="trivia-item__button">{incorrectAnswers[1]}</button>
-            </li>
-            <li> <button id="trivia-item__button">{incorrectAnswers[2]}</button> </li>
+                );
+            })}
+            
         </ul>
     </div>
 }
