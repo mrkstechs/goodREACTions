71b0ff483ec3cd51170386c51e7dd82c1ff633ee
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-08 16:11:44 +0000

    Test now appears in coverage

--- server.test.js
+++ server.test.js
@@ -1,6 +1,8 @@
 const io = require("socket.io-client");
+require("./index")
 
 describe("Server", () => {
+
     test("creates a lobby and checks that the server emits the correct messages", (done) => {
         const socket = io("http://localhost:2333");
       
@@ -12,15 +14,12 @@ describe("Server", () => {
       
           expect(message).toBe(`Created lobby. LobbyId: my-lobby`);
       
-          // Wait for the "send-to-lobby" event to be emitted by the server
           socket.on("send-to-lobby", (lobbyId, username, userList, gameHost) => {
-            // Check that the server emitted the correct values
             expect(lobbyId).toBe("my-lobby");
             expect(username).toBe("my-username");
             expect(userList).toEqual(["my-username"]);
             expect(gameHost).toBe(socket.id);
       
-            // Close the connection and finish the test
             socket.close();
             done();
           });
