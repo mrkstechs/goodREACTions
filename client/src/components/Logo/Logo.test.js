3dff8f6f6a488810c89993cb8529f864c09e449e
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-12-08 15:33:32 +0000

    Logo tests

---
+++ client/src/components/Logo/Logo.test.js
@@ -0,0 +1,19 @@
+import { screen, render } from '@testing-library/react';
+import Logo from '.'
+
+describe('Logo', () =>{
+    beforeEach(() =>{
+      render(<Logo/>)
+    })
+
+    test('image of logo exists', ()=>{
+        const img = screen.getByAltText("Welcome to Quizzic")
+        expect(img).toBeInTheDocument()
+    })
+
+    test('Logo contains div', () =>{
+        const {container} = render(<Logo/>)
+        const div = container.getElementsByClassName('logo')
+        expect(div.length).toBe(1)
+    })
+})
