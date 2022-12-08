e4c51dc45b97fd1f7cb58573232afdf8483bfabf
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-12-08 17:57:30 +0000

    Homepage testing

--- client/src/pages/Homepage/Homepage.test.js
+++ client/src/pages/Homepage/Homepage.test.js
@@ -1,5 +1,6 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, queryByAttribute } from '@testing-library/react';
 import Homepage from '.'
+import Popup from '.'
 import {BrowserRouter as Router} from 'react-router-dom';
 
 describe('Homepage', () =>{
@@ -25,12 +26,34 @@ describe('Homepage', () =>{
         expect(leaderboardLobby.textContent).toBe("Leaderboard")
     })
 
-    test('It has a form', () =>{
-        const form = screen.getByRole('form');
-        expect(form).toBeInTheDocument();
-    })
+    // test('It has a form', () =>{
+    //     const getById = queryByAttribute.bind(null, 'id')
+    //     const container = render(<Popup/>, { wrapper: Router })
+    //     const form = getById(container.container, 'form')
+    //     expect(form).toBeInTheDocument();
+    // })
 
-    test('Each Input has a label', () =>{
-        
+    // test('div containing form', () =>{
+    //     const {container} = render(<Popup/>, { wrapper: Router })
+    //     const div = container.getElementsByClassName('popupForm')
+
+    //     expect(div).toBeInTheDocument()
+    // })
+
+    test('Header components can be seen', () =>{
+        const username = screen.getByRole('img', { name: 'Welcome to Quizzic'})
+        expect(username).toBeInTheDocument()
     })
+    // test('form has title', () =>{
+    //     const heading = screen.getByRole('heading', { name: 'Create Lobby'})
+        
+    //     expect(heading).toBeInTheDocument()
+    //     expect(heading.textContent).toBe('Create Lobby')
+    // })
+    // test('Popup', () =>{
+    //     const container = render(<Popup/>, { wrapper: Router})
+    //     const header = container.getByRole('heading')
+
+    //     expect(header).toBeInTheDocument()
+    // })
 })
