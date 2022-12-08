46532b99d0899b915f96897f84a0d9290b42bc02
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-08 22:17:17 +0000

    Additional testing

--- socket.test.js
+++ socket.test.js
@@ -1,10 +1,12 @@
 const io = require("socket.io-client");
-require("./index")
+const { fetchQuestions } = require("./index")
 
 describe("SocketServer", () => {
+   
     test("creates a lobby and checks that the server emits the correct messages", (done) => {
+    
         const socket = io("http://localhost:2333");
-      
+
         socket.on("connect", () => {
           socket.emit("create-lobby", "my-lobby", "my-username");
         });
@@ -24,4 +26,96 @@ describe("SocketServer", () => {
           });
         });
       });
+
+    test("Recieves userList on get-user-list emit", () => {
+
+        const socket = io("http://localhost:2333");
+
+        socket.on("connect", () => {
+            socket.emit("get-user-list", socket.id)
+        });
+
+        socket.emit("get-user-list", socket.id)
+
+        socket.on("send-user-list", (userList) => {
+            expect(userList).toBe(socket.id)
+        })
+    })
+
+    test("Start game function works and gets questions from API", () => {
+        const socket = io("http://localhost:2333");
+
+        const options = {
+            category: "any",
+            difficulty: "any",
+            timer: "30",
+            maxPlayers: "4",
+            numQuestions: "10"
+        }
+
+        socket.on("connect", () => {
+            socket.emit("create-lobby", "my-lobby", "my-username");
+
+            socket.on("start-game", (lobbyId, recievedOptions) => {
+
+                expect(lobbyId).toEqual("my-lobby");
+                expect(recievedOptions).toEqual(options);
+
+                socket.emit("start-game", "my-lobby", options)
+            })
+
+        });
+    
+        socket.on("game-starting", (questionData) => {
+            const lobby = server.sockets.adapter.rooms.get(lobbyId)
+
+            expect(lobby.activePlayerTracker).toBe(0)
+            expect(lobby.activePlayer).toBeDefined()
+            expect(lobby.options).toBeDefined()
+            expect(lobby.questions).toBeDefined()
+            expect(questionData).toBeDefined()
+        })
+    })
+
+
+    test("initialises game", () => {
+
+        const options = {
+            category: "any",
+            difficulty: "any",
+            timer: "30",
+            maxPlayers: "4",
+            numQuestions: "10"
+        }
+        
+        const socket = io("http://localhost:2333");
+
+        socket.on("connect", () => {
+            socket.emit("create-lobby", "my-lobby", "my-username");
+            socket.emit("start-game", "my-lobby", options)
+            socket.emit("init-game", "my-lobby");
+        });
+
+        socket.on("send-question", (question, activePlayer) => {
+            expect(question).toBeDefined()
+            expect(activePlayer).toBe(socket.id)
+        })
+    })
+
+    test("Generates correct url for API call", async () => {
+
+        const options = {
+            category: "any",
+            difficulty: "any",
+            timer: "30",
+            maxPlayers: "4",
+            numQuestions: "10"
+        };
+
+        const players = ['player1', 'player2'];
+        
+        const questions = await fetchQuestions(options, players);
+        expect(questions).toBeDefined();
+        expect(questions.length).toEqual(5 * players.length);
+    })
 })
